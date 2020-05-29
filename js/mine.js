54ed98cdd2c25d4a178c28a5be5c5959b9a9c244
Ahmed Nasser <ahmed.nasser@webgenstudio.com>
2020-05-30 00:09:30 +0200

    add jquery and use ajax in get & delete requests

--- js/mine.js
+++ js/mine.js
@@ -3,25 +3,23 @@ let httpReq = new XMLHttpRequest;
 getData();
 
 function getData(){
-    httpReq.open("get", "https://jsonplaceholder.typicode.com/posts")
-    httpReq.send();
-    httpReq.onreadystatechange = function()
-    {
-        if(httpReq.readyState === 4 && httpReq.status === 200)
-        {
-           allData = JSON.parse(httpReq.response);
-           console.log(allData)
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        type: 'GET',
+        success: function(result) {
+           console.log(result);
+           allData = result;
            displayData()
         }
-    }
+    });
 }
 function displayData()
 {
     temp =``;
     for(let i=0 ; i<allData.length ; i++)
     {
-        temp+=` <div id="cols" class="col-md-3 col-sm-4 my-3 ">
-                    <div onclick=post() class="items p-3">
+        temp+=` <div id='post-${i}' id="cols" class="col-md-3 col-sm-4 my-3 ">
+                    <div class="items p-3">
                         <h3>Post ${allData[i].id}</h3>
                         <h4>${allData[i].title}</h4>
                         <p>${allData[i].body}</p>
@@ -36,8 +34,24 @@ function displayData()
 }
 function deletePost(index)
 {
-    allData.splice(index, 1);
-    displayData();
+    let postId = index + 1;
+    let URL = 'https://jsonplaceholder.typicode.com/posts/'+postId;
+    $.ajax({
+        url: URL,
+        type: 'DELETE',
+        headers: {
+            "access-control-allow-origin" : "*",
+            "Content-type": "application/json; charset=UTF-8",
+            "access-control-allow-headers": "content-type"
+        },
+        success: function(result) {
+           allData.splice(index, 1);
+           $( `#post-${index}`).remove();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+            console.log("Status: " + textStatus + "Error: " + errorThrown)
+        } 
+    });
 }
 
 
@@ -61,6 +75,7 @@ function showPost(){
 let layoutContainer = document.getElementById("layout-container");
 let close = document.getElementById("close");
 
+/*
 function post(){
     addEventListener("click" , showbox);
     function showbox(e){
@@ -76,3 +91,4 @@ function post(){
     
 
 }
+*/
